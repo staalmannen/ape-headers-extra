58f26bb1ead08d39ff14be981f1a83fbc6f51f4a
Jens Staal <staal1978@gmail.com>
2020-06-16 10:12:35 +0200

    err.h stubs

---
+++ err.h
@@ -0,0 +1,32 @@
+/* 
+ * Public domain 
+ * err.h compatibility stub
+ * libressl has a nice shim header 
+ * TODO: to make a similar shim compatible
+ * with Plan9 cpp
+ */ 
+
+#ifndef ERR_H
+#ifndef COMPAT_ERR_H 
+#define COMPAT_ERR_H 
+
+#include <errno.h> 
+#include <stdio.h> 
+#include <string.h> 
+
+#define err(...)  /* */
+
+#define errx(...) /* */
+
+#define warn(...) /* */
+
+#define warnx(...) /* */
+
+#endif /* COMPAT_ERR_H */
+#endif /* ERR_H */
+
+
+
+
+
+ 
